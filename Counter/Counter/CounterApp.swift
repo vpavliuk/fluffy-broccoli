d410926b5d7674b137811138f9747d39e9ede4c1
Volodymyr Pavliuk <gwotan@gmail.com>
2022-08-20 14:40:11 +0300

    Add xcode project

---
+++ Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Vova on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
